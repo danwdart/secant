65c270b88f58c9a52318de7cb3b5afdca6ae4a81
Kathie Dart <git@kathiedart.uk>
2016-11-24 17:02:55 +0000

    things bizatch

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -1,13 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './components/myapp';
 import { ButtonsComponent } from './components/buttons';
 import { AjaxeyComponent } from './components/ajaxey';
 
+;
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+        {
+            path: 'ajaxey/:id',
+            component: AjaxeyComponent
+        }
+    ])
+],
   declarations: [ AppComponent, ButtonsComponent, AjaxeyComponent ],
   bootstrap:    [ AppComponent ]
 })
--- public/js/src/components/ajaxey/index.js
+++ public/js/src/components/ajaxey/index.js
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { BobService }               from '../../services/bob';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'my-ajaxey',
@@ -7,4 +13,26 @@ import { Component } from '@angular/core';
 })
 export class AjaxeyComponent {
     
+    static parameters = [[BobService], [ActivatedRoute], [Location]];
+    
+    constructor(BobService, route, location) {
+        this.BobService = BobService;
+        this.route = route;
+        this.location = location;
+    }
+    
+    ngOnInit() {
+        console.log(this.BobService, this.route, this.location);
+        this.route.params.switchMap((params) => this.BobService.getBob(+params.id))
+            .subscribe(bob => this.bob = bob)
+    }
+    
+    goBack() {
+        this.location.back();
+    }
+    
+    bob = null;
+    
+    @Input()
+    id = null;
 }
